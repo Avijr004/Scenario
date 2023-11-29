7a42ae159ed810ad7d70cefa765ac04e1e8fbaeb
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-11-29 22:42:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,10 +42,10 @@ TIGERS = {int(x) for x in os.environ.get("TIGERS", "").split()}
 INFOPIC = bool(os.environ.get("INFOPIC", True)) or "https://telegra.ph/file/a9ec99487ecd550460309.jpg"
 
 # Make a new group then add @ScenarioXbot then send /id and fill id here.
-EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
+EVENT_LOGS = os.environ.get("EVENT_LOGS", "-1001603027566")
 
 # Make a new group then add @ScenarioXbot then send /id and fill id here.
-ERROR_LOGS = os.environ.get("ERROR_LOGS", None)
+ERROR_LOGS = os.environ.get("ERROR_LOGS", "-1001603027566")
 
 # Don't touch if you don't know.
 WEBHOOK = bool(os.environ.get("WEBHOOK", False))
@@ -57,13 +57,13 @@ PORT = int(os.environ.get("PORT", 8443))
 CERT_PATH = os.environ.get("CERT_PATH")
 
 # Bot Owner's API_ID (From:- https://my.telegram.org/apps)
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "23653093")
 
 # Bot Owner's API_HASH (From:- https://my.telegram.org/apps)
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "ee6df88753c36eeab95391940ba3844f")
 
 # Any SQL Database Link (RECOMMENDED:- PostgreSQL & https://www.elephantsql.com)
-DB_URL = os.environ.get("DATABASE_URL", "") 
+DB_URL = os.environ.get("DATABASE_URL", "postgres://joestroq:xFuhuUu_XRrZA-KzUkCNLWVq14qT4yIu@isabelle.db.elephantsql.com/joestroq") 
 
 # Don't touch
 DB_URL = DB_URL.replace(
@@ -71,7 +71,7 @@ DB_URL = DB_URL.replace(
 )  # rest of connection code using the connection string `uri`
 
 # Donation Link (ANY)
-DONATION_LINK = os.environ.get("https://t.me/i_14344")
+DONATION_LINK = os.environ.get("https://t.me/lochakpochak")
 
 # Wall api key for wallpapers # From:- https://wall.alphacoders.com/api.php
 WALL_API = os.environ.get("WALL_API", None) 
@@ -85,15 +85,15 @@ OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", "") # From:- https://ope
 GENIUS_API_TOKEN = os.environ.get("GENIUS_API_TOKEN", None) # From:- http://genius.com/api-clients
 MONGO_DB_URL = os.environ.get("MONGO_DB_URL", "mongodb+srv://Cluster006:600510@cluster006.ootpa.mongodb.net/Cluster006?retryWrites=true&w=majority")
 REDIS_URL = os.environ.get("REDIS_URL", "redis://Madharjoot:GuKhao123_@redis-12276.c275.us-east-1-4.ec2.cloud.redislabs.com:12276/Madharjoot")
-BOT_ID = int(os.environ.get("BOT_ID", None)) # Telegram Bot ID (EXP:- 1241223850)
+BOT_ID = int(os.environ.get("BOT_ID", 6491531689)) # Telegram Bot ID (EXP:- 1241223850)
 SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None) # Support Chat Group Link (Use @ScenarioXSupport || Dont Use https://t.me/ScenarioXSupport)
 SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None) # Use @SpamWatchSupport
 SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None) # From https://t.me/SpamWatchBot 
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # Bot Username
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "Tessa_Ro_bot") # Bot Username
 
-STRING_SESSION = os.environ.get("STRING_SESSION", None) # Telethon Based String Session (2nd ID) [ From https://repl.it/@SpEcHiDe/GenerateStringSession ]
+STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOIwBu5tshZMHbtj0ifWhcrHggvWzjyW1UPH9NVMdv-f4d43coEbQ_RdB9DF4g1Wf8kVGZ2t7cWd1V86EwzL4wP5G0CdZb3GqpYFpUk08koJH01AMihEtBB89VgNXPR7_432wdQmbN-RR5OlZJ-d93M9-ifGxzaQyHVZN43wVr9KZv2z-vQA7T2v4a-2A2D2442gMlRJ1gJJJ-6W7Jxc06kUm3d_f8BOGL34ms8dcCmg82DkbeqY2dvFutqoOYQHDZR3682I_BExkl1ELDIZZmb2wr2EUNJZq7IUL1-p9bRCZX1Iad44Vssi0A-JicNyLz8D2X54eo3a_gU36myIzNeL1G1A=") # Telethon Based String Session (2nd ID) [ From https://repl.it/@SpEcHiDe/GenerateStringSession ]
 REPO = "TeamScenario/Scenario"
-DEVELOPER = "TeamScenario"
+DEVELOPER = "lochakpochak"
 APP_ID = API_ID
 APP_HASH = API_HASH
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", True) # Heroku App Name 
